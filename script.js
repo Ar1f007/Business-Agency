63fc893380707571cd76a5b47df85e896e34ef14
Ar1f007 <muhammadarif9416@yahoo.com>
2021-04-16 04:54:14 +0600

    pricing cards swipe using Swiper JS

--- script.js
+++ script.js
@@ -41,3 +41,19 @@ video.addEventListener("timeupdate", () => {
   }
 });
 // End of Section 2 video
+
+// Pricing Cards Section 3
+var swiper = new Swiper(".swiper-container", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  coverflowEffect: {
+    rotate: 70,
+    stretch: 0,
+    depth: 100,
+    modifier: 1,
+    slideShadows: true,
+  },
+});
+// End of Pricing Cards Section 3
